5d7ccddb8c38ab1ed8f7b06860cf85a3a5c3b70c
morozov <x17animi13@yandex.ru>
2025-11-24 16:09:45 +0500

    result-3 task 3

---
+++ src/main/kotlin/lessons_3/task_4.kt
@@ -0,0 +1,24 @@
+package org.example.lessons_3
+
+fun main(){
+
+    //Задача не понятная
+
+    val shablon = "[E2-E4;2]"
+
+    var shablonBez = shablon.replace("[","")
+    shablonBez = shablonBez.replace("]","")
+
+    val posSeporator = shablonBez.indexOf("-")
+    val posStep = shablonBez.indexOf(";")
+
+    val start = shablonBez.substring(0,posSeporator)
+    val end = shablonBez.substring(posSeporator+1, posSeporator + 3)
+    val hod = shablonBez.substring(posStep+1)
+
+
+    println("C " + start)
+    println("Na " + end)
+    println("xod " + hod)
+
+}
