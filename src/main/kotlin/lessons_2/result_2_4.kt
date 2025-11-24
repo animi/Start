715ef9edd10a605d0de564b441011a9feea32031
morozov <x17animi13@yandex.ru>
2025-11-24 14:06:51 +0500

    result-2-1 task 4 (tyt 4 oshibsya)

---
+++ src/main/kotlin/lessons_2/result_2_4.kt
@@ -0,0 +1,14 @@
+package org.example.lessons_2
+
+fun main(){
+
+    val kristal = 7
+    val jelezo = 11
+
+
+    println( "Кристал бонус: " +  (kristal * 0.2).toInt())
+    println( "Железо бонус: " +  (jelezo * 0.2).toInt())
+
+
+}
+
